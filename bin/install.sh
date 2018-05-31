ba25bb2916b67b4fede59ae4151affe5cad5991b
yhkaplan <yhkaplan@gmail.com>
2018-05-31 16:47:51 +0900

    swift package fetch was deprecated, so switched to resolve

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 brew install cloc
-swift package fetch
+swift package resolve
 swift build -c release -Xswiftc -static-stdlib
 cd .build/release
 cp -f LattnerBot /usr/local/bin/lattnerbot
